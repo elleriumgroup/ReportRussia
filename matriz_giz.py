e0672c830cc42a07bf5aa4348c564645aa0881cc
maksdev <maksdev@bk.ru>
2021-02-10 15:41:30 +0300

    init 0000

--- matriz_giz.py
+++ matriz_giz.py
@@ -48,12 +48,12 @@ def scanline(s):
                 mas[x] = int(dline[x])
     # формируем строку вставки в файл
     s = f'''insert into matrix(oid, form, myear, mperiod, r, s,
-                               g01, g02, g03, g04, g05, g06, g07, g08, g09, g10, g11, g12, g13, g14, g15, g16, g17, g18, g19, g20,
-                               g21, g22, g23, g24, g25, g26, g27, g28, g29, g30, g31, g32, g33, g34, g35, g36, g37, g38, g39, g40,
-                               g41, g42, g43, g44, g45, g46, g47, g48, g49, g50, g51, g52, g53, g54, g55, g56, g57, g58, g59, g60,
-                               g61, g62, g63, g64, g65, g66, g67, g68, g69, g70, g71, g72, g73, g74, g75, g76, g77, g78, g79, g80,
-                               g81, g82, g83, g84, g85, g86, g87, g88, g89, g90, g91, g92, g93, g94, g95, g96, g97, g98, g99) values (
-                               '{oid}', {form}, {myear}, {mperiod}, {r}, {mas[0]},
+        g01, g02, g03, g04, g05, g06, g07, g08, g09, g10, g11, g12, g13, g14, g15, g16, g17, g18, g19, g20,
+        g21, g22, g23, g24, g25, g26, g27, g28, g29, g30, g31, g32, g33, g34, g35, g36, g37, g38, g39, g40,
+        g41, g42, g43, g44, g45, g46, g47, g48, g49, g50, g51, g52, g53, g54, g55, g56, g57, g58, g59, g60,
+        g61, g62, g63, g64, g65, g66, g67, g68, g69, g70, g71, g72, g73, g74, g75, g76, g77, g78, g79, g80,
+        g81, g82, g83, g84, g85, g86, g87, g88, g89, g90, g91, g92, g93, g94, g95, g96, g97, g98, g99) values (
+                              '{oid}', {form}, {myear}, {mperiod}, {r}, {mas[0]},
                                {mas[1]},  {mas[2]},  {mas[3]},  {mas[4]},  {mas[5]},  {mas[6]},  {mas[7]},  {mas[8]},  {mas[9]},  {mas[10]},
                                {mas[11]}, {mas[12]}, {mas[13]}, {mas[14]}, {mas[15]}, {mas[16]}, {mas[17]}, {mas[18]}, {mas[19]}, {mas[20]},
                                {mas[21]}, {mas[22]}, {mas[23]}, {mas[24]}, {mas[25]}, {mas[26]}, {mas[27]}, {mas[28]}, {mas[29]}, {mas[30]},
@@ -64,7 +64,6 @@ def scanline(s):
                                {mas[71]}, {mas[72]}, {mas[73]}, {mas[74]}, {mas[75]}, {mas[76]}, {mas[77]}, {mas[78]}, {mas[79]}, {mas[80]},
                                {mas[81]}, {mas[82]}, {mas[83]}, {mas[84]}, {mas[85]}, {mas[86]}, {mas[87]}, {mas[88]}, {mas[89]}, {mas[90]},
                                {mas[91]}, {mas[92]}, {mas[93]}, {mas[94]}, {mas[95]}, {mas[96]}, {mas[97]}, {mas[98]}, {mas[99]});'''
-    # return(s.rstrip('\n'))
     return s
 
 
